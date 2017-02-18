6a8c0a10d8f449d73be7bcd127ffd162a2a51e2e
Stefan Walther <Stefan.Walther@qlik.com>
2017-02-18 01:05:03 +0100

    feat: Initial methods

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const HttpStatus = require('http-status-codes');
 
 class ExpressStatus {
 
+  static ok(res, result) {
+    res.status(HttpStatus.OK).json(result);
+  }
+
+  static created(res, result) {
+    res.status(HttpStatus.CREATED).json(result);
+  }
+
+  static error(res, err) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
+  }
 
 }
 
