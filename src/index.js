20d419e9d812a5dbec16eeb25a7547f39e2d089a
Stefan Walther <swr@qlik.com>
2018-03-30 19:41:04 +0200

    docs: some basic documentation

--- src/index.js
+++ src/index.js
@@ -3,19 +3,47 @@ const HttpStatus = require('http-status-codes');
 // Todo: Just found express-status, which some similar functionality, have a look ...
 class ExpressStatus {
 
+  /**
+   *
+   * @param res
+   * @param result
+   *
+   * @static
+   */
   static ok(res, result) {
     res.status(HttpStatus.OK).json(result);
   }
 
+  /**
+   *
+   * @param res
+   * @param result
+   *
+   * @static
+   */
   static created(res, result) {
     res.status(HttpStatus.CREATED).json(result);
   }
 
+  /**
+   *
+   * @param res
+   * @param err
+   *
+   * @static
+   */
   static error(res, err) {
     res.setHeader('Content-Type', 'application/json');
     res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
   }
 
+  /**
+   *
+   * @param res
+   * @param json
+   *
+   * @static
+   */
   static json(res, json) {
     res.setHeader('Content-Type', 'application/json');
     res.status(HttpStatus.OK).json(json);
